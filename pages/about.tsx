adf7afc90ff0c98c657e49c90612831a7e02c1d1
dmitry <galionix2@gmail.com>
2023-01-23 21:15:27 +0200

    update about

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
-import styles from '@/styles/Home.module.scss';
+import styles from '@/styles/About.module.scss';
 import { AboutLayout, DefaultLayout } from '../src/components/Layout';
 import Link from 'next/link';
 
@@ -10,8 +10,13 @@ const inter = Inter({ subsets: ['latin'] });
 export default function About() {
   return (
     <AboutLayout>
-      <Link href='/'>Main</Link>
-      <Link href='/about'>About</Link>
+      <section className={styles.about}>
+        <h1>About</h1>
+        <p>
+          Hello! The purpose of this project is to provide easier way to share
+          my tracks with people. Hope you like them!
+        </p>
+      </section>
     </AboutLayout>
   );
 }
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,20 +1,29 @@
 import s from './Menu.module.scss';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export const Menu = () => {
+  const router = useRouter();
+  console.log('router: ', router);
+
+  const isActive = (path: string) => {
+    return router.pathname === path;
+  };
   return (
     <section className={s.menu}>
       <ul>
-        <li>
+        <li className={isActive('/') ? s.active : ''}>
           <Link href='/'>Playlist</Link>
         </li>
-        <li>
+        <li className={isActive('/collections') ? s.active : ''}>
           <Link href='/collections'>Collections</Link>
         </li>
-        <li>
+        <li className={isActive('/about') ? s.active : ''}>
           <Link href='/about'>About</Link>
         </li>
-        <li>
+        <li
+        // clas
+        >
           <a
             href='https://thedimas.com'
             rel='noopener noreferrer'
